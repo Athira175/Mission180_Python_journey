2581163a00516cb2cef2b0424d443bbcb79de110
Athira175 <75133643+Athira175@users.noreply.github.com>
2026-07-24 15:02:53 +0200

    Day13 OOP Employee class

---
+++ 01_Python/Day13/employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self,name,roll,salary):
+        self.name=name
+        self.roll=roll
+        self.salary=salary
+emply=Employee("Athira","Developer",50000)
+print("EMployee Name:",emply.name)
+print("Employee Roll:",emply.roll)
+print("Salary:",emply.salary)
